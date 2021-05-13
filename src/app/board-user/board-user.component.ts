8c78df4c2e59a273efde5cf2d95b42e2c838ebf6
Halley <halley.is.me@gmail.com>
2021-05-13 14:06:20 -0500

    login and logout work, added some styles

--- src/app/board-user/board-user.component.ts
+++ src/app/board-user/board-user.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { Project } from 'src/app/models/project.model';
+import { ProjectService } from 'src/app/services/project.service';
+import { ProjectListComponent } from 'src/app/project-list/project-list.component';
 
-@Component({
+@Component({ 
   selector: 'app-board-user',
   templateUrl: './board-user.component.html',
   styleUrls: ['./board-user.component.css']
 })
 export class BoardUserComponent implements OnInit {
   content?: string;
+ 
 
   constructor(private userService: UserService) { }
 
